59bc3dec31c4c160c7181d693ec5f57ec74a1417
Ashish <ashishtaduce2@gmail.com>
2020-07-06 10:54:27 +0530

    Class cant be assigned to image div and code block

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, {useState} from 'react';
 import './App.scss';
+import CodeBlock from "./Components/code-block/code-block";
 
 function App() {
     let [input, setInput] = useState('');
-    let lines = Array(input.split('\n').length).fill(1);
-    let lineCount = lines.map((line, index) => `#${index + 1}\n`);
-    lineCount = lineCount.join('');
-    let lineNumber = lineCount;
-    // console.log(lineNumber);
 
     function processInput(input) {
         let captureNewLines = new RegExp(/(^\n)/, "gm");
@@ -16,7 +12,16 @@ function App() {
         let captureHeader1 = new RegExp(/^# (.*)/, "gm");
         let captureLine = new RegExp(/(^---\n)/, "gm");
         let captureListItem = new RegExp(/^- (.*)/, "gm");
+        let captureImages = new RegExp(/^! (\[.*\])(\(.*\))/, "gm");
+        let captureCode = new RegExp(/^`.*`/, "gm");
 
+        input = input.replace(captureImages, (capturedText, group1, group2) => {
+            let link = group2.toString().substring(1, group2.length - 1);
+            let name = group1.toString().substring(1, group1.length - 1);
+            console.log(name, link);
+            return `<img src= ${link} alt= ${name} className = {"image-div"}/>`;
+        });
+        input = input.replace(captureCode, (capturedText, group1) => `<CodeBlock className={"code-block"} >${capturedText}</CodeBlock>`);
         input = input.replace(captureHeader3, (capturedText, group1) => `<h3>${group1}</h3>`);
         input = input.replace(captureHeader2, (capturedText, group1) => `<h2>${group1}</h2>`);
         input = input.replace(captureHeader1, (capturedText, group1) => `<h1>${group1}</h1>`);
@@ -27,15 +32,26 @@ function App() {
     }
     function preetifyInput(input) {
         input = input.split(`\n`);
+
         input = input.map(element => {
+            if(element.substring(0,2) === ("! ")) return <div className={"image preety-div"}>{element}</div>
+            if(element[0] === ("`") && element[element.length-1] === ("`")) return <div className={"code preety-div"}>{element}</div>
             if(element.substring(0,2) === ("# ")) return <div className={"header preety-div"}>{element}</div>
+            if(element.substring(0,3) === ("## ")) return <div className={"header preety-div"}>{element}</div>
+            if(element.substring(0,4) === ("### ")) return <div className={"header preety-div"}>{element}</div>
             if(element.substring(0,4) ===("---")) return <div className={"horizontal-line preety-div"}>{element}</div>
-            if(element === "") return <p className={"preety-div"}> </p>
-            if(element.substring(0,2) === ("* ")) return <div className={"list-item preety-div"}><span style={{color: "#3ff1f1"}}>*</span> {element.substring(1)}</div>
+            if(element === "") return <div className={"preety-div empty-div"}>{"Empty Line Here"}</div>
+            if(element.substring(0,2) === ("* ")) return <div className={"list-item preety-div"}><span style={{color: "#66d9ef"}}>*</span> {element.substring(1)}</div>;
+
             return <div className={"preety-div"}>{element}</div>
         });
         // console.log("After: ",input);
         return input;
+    }
+
+    function processImage(image_data){
+        if(image_data.includes())
+        return <div className={"image preety-div"}>{image_data}</div>
     }
   return (
     <div className="App">
---
+++ src/Components/code-block/code-block.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./_code-block.scss"
+
+export default function CodeBlock(prop) {
+    console.log(prop, "AAAAAAAAA")
+    return (
+        <div className="code-block">
+            jk
+        </div>
+    );
+}
